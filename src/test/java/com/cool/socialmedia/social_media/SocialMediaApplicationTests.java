7e0a0e6913288287c8a03fdea7bd60b3cdc33dc2
Suryansh <heresuryanshsingh@gmail.com>
2025-12-28 20:49:24 +0530

    first commit

---
+++ src/test/java/com/cool/socialmedia/social_media/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cool.socialmedia.social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
